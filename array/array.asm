5899f1bd83b56b4beeead6f429c76f8370b2a877
Gustavo Santos <gustavo.rodrigues.santos@senado.leg.br>
2023-02-03 13:37:39 -0300

    fixing

--- array/array.asm
+++ array/array.asm
@@ -5,23 +5,23 @@ section .text
     
     mov esi, 0
     print:
-		mov eax, 4									; syscall to write
-		mov ebx, 1									; write to stdout
+		mov eax, 4									                ; syscall to write
+		mov ebx, 1									                ; write to stdout
         
-        mov ecx, array                              ; pointer to output buffer
-        add ecx, esi                                ; sum the esi to array move through array
-        
-        mov edx, 1                                  ; number of bytes to write
-        int 0x80                                    ; make the interruption    
-        
-        inc esi                                     ; increment esi
-        cmp esi, [len]                              ; compare
-        jl  print 							        ; jump to for if < 10
+    mov ecx, array                              ; pointer to output buffer
+    add ecx, esi                                ; sum the esi to array move through array
+    
+    mov edx, 1                                  ; number of bytes to write
+    int 0x80                                    ; make the interruption    
+    
+    inc esi                                     ; increment esi
+    cmp esi, [len]                              ; compare
+    jl  print 							                    ; jump to for if < 10
 
     exit:
-		mov ebx, 0									; error code 0
-		mov eax, 1									; syscall to exit
-		int 0x80									; make the interruption
+		mov ebx, 0									                 ; error code 0
+		mov eax, 1									                 ; syscall to exit
+		int 0x80									                   ; make the interruption
 
 
 section .data
